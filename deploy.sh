e70630e5667c49c1cc411efc748bf2197a5d9fba
bayramkarahan <bayramk@gmail.com>
2023-10-21 19:32:56 +0300

    deneme

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -ex
 # install dependencies
-if which apt &>/dev/null && [[ -d /var/lib/dpkg && -d /etc/apt ]] ; then
+
      apt-get update
      echo "işlem başladı....."
      apt install mtools make python3 make -y
      pip3 install sphinx-build
      pip3 install rst2pdf
      pip3 install sphinx-sitemap
-fi
+
 #apt install python3 python make
 
 # install font
@@ -17,7 +17,7 @@ fi
 #mv dejavu-*/ttf /usr/share/fonts/
 #fc-cache -fv
 # build
-sh ./build.sh
+sh build.sh
 mkdir /output -p
 cp -r build/pdf/* /output
 cp -r build/html/* /output
